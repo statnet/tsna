9e47f456fde8f6b2be6c0dea162d704792a5382f
Skye Bender-deMoll <skyebend@uw.edu>
2013-04-19 05:23:08 +0000

    added maintainer (me) so it would pass checks, but I'm hoping someone else will be maintainer got forward.reachable kind of working for non-discrete case
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/tsna/trunk@11960 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/reachable.R
+++ R/reachable.R
@@ -2,7 +2,7 @@
 
 # find the set of vertices reachable from seed vertex within time range
 
-forward.reachable<-function(nd,v,start=NULL,end=NULL,per.step.depth=1 ){
+forward.reachable<-function(nd,v,start=NULL,end=NULL,per.step.depth=Inf ){
   if(!is.networkDynamic(nd)){
     stop('the first argument to forward.reachble must be a networkDynamic object')
   }
@@ -11,9 +11,6 @@ forward.reachable<-function(nd,v,start=NULL,end=NULL,per.step.depth=1 ){
   }
   if(max(v)>network.size(nd) | min(v)<1){
     stop('v argument to forward.reachable must be a vector of valid numeric vertex ids')
-  }
-  if(!per.step.depth%in%c(1,Inf)){
-    stop('forward.reachable currently only supports per.step.depth arguments of 1 and Inf')
   }
   
   times <-get.change.times(nd,vertex.attribute.activity=FALSE,edge.attribute.activity=FALSE,network.attribute.activity=FALSE)
@@ -32,17 +29,23 @@ forward.reachable<-function(nd,v,start=NULL,end=NULL,per.step.depth=1 ){
   
   
   reached <-v
-  for(t in times){
+  for(t in 1:length(times)){
     # BFS to depth rate
     # find out-connected vertices
-    ngs<-unique(sapply(v,function(i){get.neighborhood.active(nd,v=i,at=t,type='out')}))
+    ngs<-unlist(unique(sapply(reached,function(i){get.neighborhood.active(nd,v=i,at=times[t],type='out')})))
     new<-setdiff(ngs,reached)
     reached<-c(reached,new)
+    # how long until next change?
+    duration<-0
+    if(t<length(times)){
+      duration<-times[t+1]-times[t]
+    }
     # remove any in the set we've already visited
-    if (per.step.depth > 1 & length(new)>0){
-      d<-1
-      while(d <= per.step.depth){
-        ngs<-unique(sapply(new,function(i){get.neighborhood.active(nd,v=i,at=time,type='out')}))
+    if (duration>0 &length(new)>0){
+      d<-1 # we are assuming all geodesic steps count as 1, harder if we calc per edge..
+      # keep searching until we reach bounds or run out of verts to find
+      while(d <= per.step.depth*duration){
+        ngs<-unlist(unique(sapply(new,function(i){get.neighborhood.active(nd,v=i,at=times[t],type='out')})))
         new<-setdiff(ngs,reached)
         if(length(new)==0){
           break # no more verts to find
