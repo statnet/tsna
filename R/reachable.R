26ae6ed8267b5407ad7b2a5f66e3f54a1b1c5df8
Skye Bender-deMoll <skyebend@uw.edu>
2014-03-25 01:15:28 +0000

    added paths.fwd.earliest, moody's suggestion for DFS search with is really fast. with draft docs and tests.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/tsna/trunk/tsna@12708 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/reachable.R
+++ R/reachable.R
@@ -6,14 +6,16 @@ forward.reachable<-function(nd,v,start=NULL,end=NULL,per.step.depth=Inf ){
   if(!is.networkDynamic(nd)){
     stop('the first argument to forward.reachble must be a networkDynamic object')
   }
-  if(!is.numeric(v)){
+  if(missing(v) || !is.numeric(v)){
     stop('v argument to forward.reachable must be a vector of valid numeric vertex ids')
   }
   if(max(v)>network.size(nd) | min(v)<1){
-    stop('v argument to forward.reachable must be a vector of valid numeric vertex ids')
+    stop('v argument to forward.reachable must be a vector of numeric vertex ids within the range of the network size')
   }
   
+  # set the interval to be whatever the observed changes are
   times <-get.change.times(nd,vertex.attribute.activity=FALSE,edge.attribute.activity=FALSE,network.attribute.activity=FALSE)
+
   if(length(times)==0){
     times <-c(0,Inf)
   }
@@ -29,6 +31,9 @@ forward.reachable<-function(nd,v,start=NULL,end=NULL,per.step.depth=Inf ){
   times<-unique(c(start,times[times>=start]))
   times<-unique(c(times[times<=end],end))
   
+  distance<-rep(Inf,network.size(nd))
+  distance[v]<-times[1]
+  
   #TODO: could probably skip all times earlier that the active times in v?
   reached <-v
   for(t in 1:(length(times)-1)){
@@ -41,8 +46,63 @@ forward.reachable<-function(nd,v,start=NULL,end=NULL,per.step.depth=Inf ){
     if (duration>0){
       d<-1 # we are assuming all geodesic steps count as 1, harder if we calc per edge..
       # keep searching until we reach bounds or run out of verts to find
-      while(d <= per.step.depth*duration){
+      # also stop if we find all the vertices
+      while(d <= per.step.depth*duration & length(reached)<network.size(nd)){
         ngs<-unlist(unique(sapply(new,function(i){get.neighborhood.active(nd,v=i,at=times[t],type='out')})))
+        new<-setdiff(ngs,reached)
+        if(length(new)==0){
+          break # no more verts to find
+        }
+        distance[new]<-times[t]
+        reached<-c(reached,new)
+        d<-d+1
+      }
+    }
+  }
+  return(reached)
+}
+
+forward.reachable2<-function(nd,v,start=NULL,end=NULL,interval='changes',per.step.depth=Inf ){
+  if(!is.networkDynamic(nd)){
+    stop('the first argument to forward.reachble must be a networkDynamic object')
+  }
+  if(missing(v) || !is.numeric(v)){
+    stop('v argument to forward.reachable must be a vector of valid numeric vertex ids')
+  }
+  if(max(v)>network.size(nd) | min(v)<1){
+    stop('v argument to forward.reachable must be a vector of numeric vertex ids within the range of the network size')
+  }
+  
+  # if start or end is missing set the interval to be whatever the observed changes are
+  if (is.null(start)){
+    start <-min(get.change.times(nd,vertex.attribute.activity=FALSE,edge.attribute.activity=FALSE,network.attribute.activity=FALSE))
+  }
+  if (is.null(end)){
+    end <-max(get.change.times(nd,vertex.attribute.activity=FALSE,edge.attribute.activity=FALSE,network.attribute.activity=FALSE))
+  }
+    
+  # lets not loop for ever!
+  if (is.infinite(start) | is.infinite(end)){
+    stop("start and end values cannot be infinite because search will not terminate")
+  }
+  
+  #TODO: could probably skip all times earlier that the active times in v?
+  reached <-v
+  now<-start
+  while(now<end){
+    # BFS to depth rate
+    new<-reached
+    # how long until next change?
+    nextTime<-when.next.edge.change(nd,at=now,v=reached)
+    duration<-nextTime-now
+    
+    # remove any in the set we've already visited
+    if (duration>0){
+      d<-1 # we are assuming all geodesic steps count as 1, harder if we calc per edge..
+      # keep searching until we reach bounds or run out of verts to find
+      # also stop if we find all the vertices
+      while(d <= per.step.depth*duration & length(reached)<network.size(nd)){
+        ngs<-unlist(unique(sapply(new,function(i){get.neighborhood.active(nd,v=i,at=now,type='out')})))
         new<-setdiff(ngs,reached)
         if(length(new)==0){
           break # no more verts to find
@@ -51,6 +111,204 @@ forward.reachable<-function(nd,v,start=NULL,end=NULL,per.step.depth=Inf ){
         d<-d+1
       }
     }
+    # update time
+    now<-nextTime
   }
   return(reached)
 }
+
+
+# compute the forward-shortest path with a Dijkstra-style earch
+# search stats from vertex v
+# temporal search is bounded by 'start' and 'end' times. 
+
+# TODO: add param for a set of alters and option to stop when they have been reached?
+
+paths.fwd.earliest<-function(nd,v,start,end,active.default=TRUE,graph.step.time=0){
+  
+  if (!is.networkDynamic(nd)){
+    stop('to be able to calculate forward paths, the first argument must be a networkDynamic object')
+  }
+  if (missing(v) || !is.numeric(v)){
+    stop("a 'v' argument with valid vertex ids was not given to specify starting vertex")
+  }
+  if (missing(start)){
+     # TODO: use obs.period if it exists
+      changes<-get.change.times(nd)
+      if(length(changes)>0){
+        start<-min(changes)
+        # message("'start' parameter was not specified, using value first network change '",start)
+      } else {
+        # can't use inf, because all distances will be inf
+        start<-0
+        message("'start' time parameter for paths was not specified, no network changes found,  using start=",start)
+      }
+  }
+  if (missing(end)){
+    # TODO: use obs.period if it exists
+    end<-Inf
+  }
+  
+  
+  # TODO: self-loop behavior?
+  # TODO: multiplex behavior?
+  
+  dist<-rep(Inf,network.size(nd))
+  previous<-numeric(network.size(nd)) # array used for reconstructing the path
+  dist[v]<-0
+  toCheck<-rep(TRUE,network.size(nd))
+  while(sum(toCheck)>0){
+    minToCheck<-which.min(dist[toCheck])
+    # have to translate index found back
+    u<-which(toCheck)[minToCheck]
+    toCheck[u]<-FALSE
+    if (dist[u]>= end){
+      break;  # no more vertices are reachable from v within time range
+    }
+    
+    nghE<-get.edgeIDs(nd,v=u,neighborhood='out') # check neighbors of u
+    for (e in nghE){
+      w <- ifelse(nd$mel[[e]]$inl==u,nd$mel[[e]]$outl,nd$mel[[e]]$inl)   
+      # we ignore graph hop time
+      # so "distance" is how long we have to wait from 'now' until onset of edge
+      spls<-nd$mel[[e]]$atl$active
+      if (is.null(spls)){ # handle possibly missing activity value, assume always active
+        if (active.default){
+          dist_u_w<-0 #TODO: need to check active default here to know if returning Inf or dist[w]
+        } else {
+          dist_u_w<-Inf
+        }
+        
+      } else {
+        # need to include
+        splIndex<-spells.hit(needle=c(start+dist[u],end),haystack=spls)
+        
+        if (splIndex<0){
+          dist_u_w<-Inf  # vertex is never reachable in the future / within time bound
+        } else {
+         # otherwise distance is the later of dist[u] or the onset of the edge
+         dist_u_w<-max(0,(spls[splIndex,1]-start)-dist[u]+graph.step.time)
+        }
+      }
+      dist_v_w <-dist[u]+dist_u_w # TODO: could add graph hop time in here
+      if (dist_v_w < dist[w]){ # if this new value is shorter, update
+        dist[w]<-dist_v_w
+        previous[w]<-u
+      }
+    }
+  }
+  return(list(distance=dist,previous=previous))
+}
+
+# I think to avoid getting into "longest path problem" territory, 
+# need to start at the end and minimize backwards
+
+paths.bkwd.latest<-function(nd,v,start,end,active.default=TRUE,graph.step.time=0){
+  
+  if (!is.networkDynamic(nd)){
+    stop('to be able to calculate forward paths, the first argument must be a networkDynamic object')
+  }
+  if (missing(v) || !is.numeric(v)){
+    stop("a 'v' argument with valid vertex ids was not given to specify starting vertex")
+  }
+  if (missing(end)){
+    # TODO: use obs.period if it exists
+    changes<-get.change.times(nd)
+    if(length(changes)>0){
+      ned<-max(changes)
+      # message("'start' parameter was not specified, using value first network change '",start)
+    } else {
+      # can't use inf, because all distances will be inf
+      start<-0
+      message("'end' time parameter for paths was not specified, no network changes found,  using end=",end)
+    }
+  }
+  if (missing(start)){
+    # TODO: use obs.period if it exists
+    start<- -Inf
+  }
+  
+  
+  # TODO: self-loop behavior?
+  # TODO: multiplex behavior?
+  
+  dist<-rep(Inf,network.size(nd))
+  previous<-numeric(network.size(nd)) # array used for reconstructing the path
+  dist[v]<-0
+  toCheck<-rep(TRUE,network.size(nd))
+  while(sum(toCheck)>0){
+    minToCheck<-which.min(dist[toCheck])
+    # have to translate index found back
+    u<-which(toCheck)[minToCheck]
+    toCheck[u]<-FALSE
+    if (dist[u]>= end){
+      break;  # no more vertices are reachable from v within time range
+    }
+    
+    nghE<-get.edgeIDs(nd,v=u,neighborhood='out') # check neighbors of u
+    for (e in nghE){
+      w <- ifelse(nd$mel[[e]]$inl==u,nd$mel[[e]]$outl,nd$mel[[e]]$inl)   
+      # we ignore graph hop time
+      # so "distance" is how long we have to wait from 'now' until onset of edge
+      spls<-nd$mel[[e]]$atl$active
+      if (is.null(spls)){ # handle possibly missing activity value, assume always active
+        if (active.default){
+          dist_u_w<-0 #TODO: need to check active default here to know if returning Inf or dist[w]
+        } else {
+          dist_u_w<- Inf
+        }
+        
+      } else {
+        # can't use spells.hit because it returns earliest spell, not latest
+        splIndex<- -1
+        for (s in nrow(haystack):1) {
+          if (spells.overlap(c(start+dist[u],end), spls[s, ])) {
+            return(s)
+          }
+        }
+        
+        if (splIndex<0){
+          dist_u_w<- Inf  # vertex is never reachable in the future / within time bound
+        } else {
+          # otherwise distance is the later of dist[u] or the terminus of the edge
+          dist_u_w<-max(0,(spls[splIndex,2]-end)-dist[u]+graph.step.time)
+        }
+      }
+      dist_v_w <-dist[u]+dist_u_w 
+      if (dist_v_w < dist[w]){ # if this new value is shorter, update
+        dist[w]<-dist_v_w
+        previous[w]<-u
+      }
+    }
+  }
+  
+  # TODO: we are measuring distance backwards from the end
+  
+  return(list(distance=dist,previous=previous))
+}
+
+create_tree<-function(results){
+  distance<-results$distance
+  previous<-results$previous
+  vids<-which(distance<Inf)
+  n<-length(vids)
+  tree<-network.initialize(n,directed=TRUE)
+  network.vertex.names(tree)<-vids
+  for(v in seq_along(vids)){
+    
+    if(previous[vids[v]]!=0){ # source vertex will have previous id of 0, so out of range
+      fromId<-match(previous[vids[v]],vids)
+      add.edges.active(tree,tail=fromId,head=v,onset=distance[vids[v]],terminus=Inf)
+    }
+  }
+  return(tree)
+}
+
+# compute the sets of vertices reachable from each vertex on the graph
+reachable_sets<-function(nd,direction='fwd'){
+  sizes<-sapply(seq_len(network.size(nd)),function(v){
+    sum(paths.fwd.earliest(nd,v=v)$distance<Inf)
+    })
+  return(size)
+}
+
--- tests/reachable_tests.R
+++ tests/reachable_tests.R
@@ -1,10 +1,11 @@
 # test the forward and backward componetnt functions
 require(tsna)
 require(testthat)
+require(networkDynamicData)
 linegraph<-network.initialize(10)
 add.edges(linegraph,tail=1:9,head=2:10)
 
-
+# ---- forward.reachable tests -----
 # test non timed version error
 test<-linegraph
 expect_error(forward.reachable(test,v=1),'must be a networkDynamic')
@@ -67,6 +68,193 @@ test_that("network with at spell durations",{
   
 })
 
+line<-network.initialize(4)
+add.edges.active(line,tail=1:3,head=2:4,onset=c(1,0,2),terminus=c(Inf,1,2))
+forward.reachable(line,v=1)
+paths.fwd.earliest(line,v=1)
 
 # test on network with net.obs.period set
+
+# test on network with way too many time steps
+data(hospital_contact)
+#forward.reachable(hospital,v=1,start=120, end=347640,interval=300,per.step.depth=1)
+
+
+# test that it matches infction in example network
+
+data(concurrencyComparisonNets)
+which(get.vertex.attribute.active(base,'status',at=1)==1)
+# size of forward component in base from v 24
+sum(get.vertex.attribute.active(base,'status',at=102)==1)
+
+epiFound<-which(get.vertex.attribute.active(base,'status',at=102)==1)
+fwdFound<-forward.reachable(base,v=24,per.step.depth=1,end=100)
+expect_equal(length(setdiff(fwdFound,epiFound)),0)
+
+# --- profiling -----#
+# fiveRuns1<-function(){
+#   forward.reachable1(base,v=1,per.step.depth=1)
+#   forward.reachable1(base,v=2,per.step.depth=1)
+#   forward.reachable1(base,v=3,per.step.depth=1)
+#   forward.reachable1(base,v=4,per.step.depth=1)
+#   forward.reachable1(base,v=5,per.step.depth=1)
+# }
+# fiveRuns2<-function(){
+#   forward.reachable2(base,v=1,per.step.depth=1)
+#   forward.reachable2(base,v=2,per.step.depth=1)
+#   forward.reachable2(base,v=3,per.step.depth=1)
+#   forward.reachable2(base,v=4,per.step.depth=1)
+#   forward.reachable2(base,v=5,per.step.depth=1)
+# }
+# 
+# # memory profiling
+# Rprof(filename='fwdReachable.before')
+# fiveRuns2()
+# Rprof(NULL)
+# summaryRprof(filename='fwdReachable.before')
+
+# # time profiling
+# library(microbenchmark)
+# timing<-microbenchmark(fiveRuns1(),fiveRuns2(),times=1)
+
+
+# ----- tests for paths.fwd.earliest ----
+
+test_that('paths.fwd.earliest basic tests',{
+  line<-network.initialize(4)
+  add.edges.active(line,tail=1:3,head=2:4,onset=0:2,terminus=1:3)
+  # check return format
+  expect_equal(names(paths.fwd.earliest(line,v=1)),c('distance','previous'))
+  
+  # check basic line
+  expect_equal(paths.fwd.earliest(line,v=1)$distance,c(0, 0, 1, 2))
+  expect_equal(paths.fwd.earliest(line,v=2)$distance,c(Inf,0,1,2))
+  
+  # test starting and ending flags
+  expect_equal(paths.fwd.earliest(line,v=1,start=0.5)$distance, c(0,0,0.5,1.5))
+  expect_equal(paths.fwd.earliest(line,v=1,start=2)$distance, c(0,Inf,Inf,Inf))
+  expect_equal(paths.fwd.earliest(line,v=1,end=2)$distance, c(0,0,1,Inf))
+  
+  line<-network.initialize(4)
+  add.edges.active(line,tail=1:3,head=2:4,onset=c(2,1,3),terminus=c(3,2,4))
+  expect_equal(paths.fwd.earliest(line,v=1)$distance,c(0,1,Inf,Inf))
+  
+  # test active default
+  test<-as.networkDynamic(network.initialize(4))
+  add.edges(test,1:3,2:4)
+  expect_equal(paths.fwd.earliest(test,v=1,start=0)$distance,c(0,0,0,0))
+  expect_equal(paths.fwd.earliest(test,v=1,active.default=FALSE,start=0)$distance,c(0,Inf,Inf,Inf))
+  
+  test<-network.initialize(4)
+  add.edges(test,1:3,3:4)
+  activate.edges(test,e=1,at=2)
+  
+  # test start message
+  test<-as.networkDynamic(network.initialize(4))
+  expect_message(paths.fwd.earliest(test,v=1),regexp="'start' time parameter for paths was not specified")
+  
+  # test wrong object
+  expect_error(paths.fwd.earliest(network.initialize(3)),regexp='first argument must be a networkDynamic object')
+  
+  # test no v specified
+  expect_error(paths.fwd.earliest(as.networkDynamic(network.initialize(2)),regexp='argument with valid vertex ids was not given'))
+  
+  # test on network size 0
+  expect_equal(paths.fwd.earliest(as.networkDynamic(network.initialize(0)),start=0,v=numeric(0))$distance,numeric(0))
+})
+
+test_that("path in large base network matches",{
+  fwdDFS<-paths.fwd.earliest(base,v=24)
+  expect_equal(sum(fwdDFS$distance<Inf),772) # should find 772 vertices, because that is what we found with BFS search
+})
+
+moodyTestNet<-network.initialize(16,directed=FALSE)
+tel<-matrix(c(674,701,1,9,
+              214,247,1,11,
+              621,651,1,12,
+              583,615,1,16,
+              749,793,11,8,
+              719,745,8,13,
+              712,739,13,5,
+              634,660,13,3,
+              769,795,13,7,
+              453,479,13,4,
+              494,524,13,2,
+              224,256,7,10,
+              40,72,10,4,
+              665,692,4,14,
+              709,740,2,15,
+              575,599,2,16,
+              748,782,4,16,
+              701,733,16,6),
+            ncol=4,byrow=TRUE)
+moodyTestNet<-networkDynamic(moodyTestNet,edge.spells=tel)
+
+# tests with moody's example network
+test_that("test of moody's example network",{
+ 
+  paths<-paths.fwd.earliest(moodyTestNet,v=10)
+  
+  expect_equal(paths$distance,c(543, 454, 594,   0, 672, 661, 184, 679, 634,   0, 709, 581, 413, 625, 669, 535))
+  expect_equal(paths$previous,c(16,13,13,10,13,16,10,13,1,0,8,1,4,4,2,2))
+  
+  # render a pdf for visual inspection of correctness
+  # tree<-create_tree(paths)
+  # pdf(file="MoodyTestNetTree.pdf",width=10,height=5)
+  # par(mfcol=c(1,2))
+  # plot(moodyTestNet,displaylabels=TRUE,
+  #      edge.label=lapply(get.edge.activity(moodyTestNet),
+  #                         function(spl){
+  #                           paste("(",spl[,1],"-",spl[,2],")",sep=
+  #                                   ''
+  #                           )
+  #                         }),
+  #      edge.label.col='blue',
+  #      edge.label.cex=0.6,
+  #      main="moody example net")
+  # 
+  # plot(tree,
+  #      coord=layout.normalize(network.layout.animate.Graphviz(tree,layout.par=list(gv.engine='dot')),keep.aspect.ratio=FALSE),
+  #      displaylabels=TRUE,
+  #      jitter=FALSE,
+  #      label.pos=2,
+  #      main='earliest paths from v10',
+  #      edge.label=lapply(get.edge.activity(tree),
+  #                        function(spl){
+  #                          paste("(",spl[,1],")",sep=
+  #                                  ''
+  #                          )
+  #                        }),
+  #      edge.label.col='blue',
+  #      edge.label.cex=0.6)
+  # par(mfcol=c(1,1))
+  # dev.off()
+})
+
+
+
+test_that("test on network with two components",{
+  test<-network.initialize(10)
+  activate.vertices(test)
+  test[1:5,5:1]<-1
+  test[6:10,10:6]<-1
+  expect_equal(which(paths.fwd.earliest(test,v=1)$distance!=Inf),1:5)
+  expect_equal(which(paths.fwd.earliest(test,v=6)$distance!=Inf),6:10)
+})
+
+
+
+# test path distance
+test_that("graph step time param works",{
+  test<-network.initialize(4)
+  add.edges.active(line,tail=1:3,head=2:4,onset=0:2,terminus=1:3)
+  # count each geodesic step as 1
+  expect_equal(paths.fwd.earliest(line,v=1,graph.step.time=1)$distance,c(0, 1, 2, 3))
+  # count each geodesic step as 2
+  expect_equal(paths.fwd.earliest(line,v=1,graph.step.time=2)$distance,c(0, 2, Inf, Inf))
+  
+})
+
+
+
              
