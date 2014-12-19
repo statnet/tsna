6f117cbc04e8bf289c912705a710e96e94924e12
Skye Bender-deMoll <skyebend@uw.edu>
2014-12-19 01:00:54 +0000

    added suggets for sna and ergm. added a function to apply ergm's static formula summary function at multiple time points #9 (Trac: 757).  And added the reachable rate function that I left out of last commit
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/tsna/trunk/tsna@13540 9cb281f4-553f-406f-8d7f-11f072bbfe46

---
+++ R/summaryStats.R
@@ -0,0 +1,34 @@
+# function to apply ergm's summary formula at multiple time points
+tSummaryStats<-function(nd, f,start, end, time.interval=1){
+    
+    if(missing(start) | missing(end)){
+      times <- get.change.times(nd)
+      if (length(times) == 0) {
+        warning("network does not appear to have any dynamic information. Using start=0 end=1")
+        start = 0
+        end = 0
+      }
+      times[times == Inf] <- NA
+      times[times == -Inf] <- NA
+      start = min(times, na.rm = T)
+      end = max(times, na.rm = T)
+    }
+    
+    # figure out the times where we will do evaluations
+    times<-seq(from = start, to=end,by = time.interval)
+    
+    # rquires that ergm is loaded
+    if(require(ergm,quietly=TRUE)){
+      stats<-lapply(times,function(t){
+        net<-network.collapse(nd,at=t)
+        summary(as.formula(paste('net',f)))
+      })
+      
+    } else {
+      stop(" the ergm package could not be loaded to provide summary functions and terms")
+    }
+    # rearrange list into matrix
+    stats<-do.call(cbind,stats)
+    colnames(stats)<-times
+    return(stats)
+}
