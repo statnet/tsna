7adcc52cd7e53beaeb8f59b1654d12104ac8eacf
Skye Bender-deMoll <skyebend@uw.edu>
2015-09-11 21:04:31 +0000

    added tests cases for each specific pShift. Fixes #26 (Trac: 1136)
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/tsna/trunk/tsna@14039 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- tests/pShift_tests.R
+++ tests/pShift_tests.R
@@ -29,4 +29,91 @@ expect_equal(pShiftCount(cls33_10_16_96,start=10,end=11), matrix(c(4,0,2,0,0,0,0
 
 
 
-# TODO: construct network that tests each shift
+#  construct network that tests each shift
+test1<-networkDynamic(edge.spells=matrix(c(0,1,1,2,
+                                           1,2,2,1),ncol=4,byrow = TRUE))
+expect_equal(as.numeric(pShiftCount(test1)),c(1,0,0,0,0,0,0,0,0,0,0,0,0))
+
+test2<-networkDynamic(edge.spells=matrix(c(0,1,1,2,
+                                           1,2,2,1,
+                                           1,2,2,3,
+                                           1,2,2,4,
+                                           1,2,2,5
+                                          ),ncol=4,byrow = TRUE))
+expect_equal(as.numeric(pShiftCount(test2)),c(0,1,0,0,0,0,0,0,0,0,0,0,0))
+
+test3<-networkDynamic(edge.spells=matrix(c(0,1,1,2,
+                                           1,2,2,3),ncol=4,byrow = TRUE))
+expect_equal(as.numeric(pShiftCount(test3)),c(0,0,1,0,0,0,0,0,0,0,0,0,0))
+
+test4<-networkDynamic(edge.spells=matrix(c(0,1,1,2,
+                                           0,1,1,3,
+                                           0,1,1,4,
+                                           1,2,2,1,
+                                           1,2,2,3,
+                                           1,2,2,4
+),ncol=4,byrow = TRUE))
+expect_equal(as.numeric(pShiftCount(test4)),c(0,0,0,1,0,0,0,0,0,0,0,0,0))
+
+test5<-networkDynamic(edge.spells=matrix(c(0,1,1,2,
+                                           0,1,1,3,
+                                           0,1,1,4,
+                                           1,2,2,1
+),ncol=4,byrow = TRUE))
+expect_equal(as.numeric(pShiftCount(test5)),c(0,0,0,0,1,0,0,0,0,0,0,0,0))
+
+test6<-networkDynamic(edge.spells=matrix(c(0,1,1,2,
+                                           0,1,1,3,
+                                           0,1,1,4,
+                                           1,2,2,3
+),ncol=4,byrow = TRUE))
+expect_equal(as.numeric(pShiftCount(test6)),c(0,0,0,0,0,1,0,0,0,0,0,0,0))
+
+test7<-networkDynamic(edge.spells=matrix(c(0,1,1,2,
+                                           1,2,3,1,
+                                           1,2,3,2,
+                                           1,2,3,4
+),ncol=4,byrow = TRUE))
+expect_equal(as.numeric(pShiftCount(test7)),c(0,0,0,0,0,0,1,0,0,0,0,0,0))
+
+test8<-networkDynamic(edge.spells=matrix(c(0,1,1,2,
+                                           1,2,3,1
+),ncol=4,byrow = TRUE))
+expect_equal(as.numeric(pShiftCount(test8)),c(0,0,0,0,0,0,0,1,0,0,0,0,0))
+
+test9<-networkDynamic(edge.spells=matrix(c(0,1,1,2,
+                                           1,2,3,2
+),ncol=4,byrow = TRUE))
+expect_equal(as.numeric(pShiftCount(test9)),c(0,0,0,0,0,0,0,0,1,0,0,0,0))
+
+test10<-networkDynamic(edge.spells=matrix(c(0,1,1,2,
+                                           1,2,3,4
+),ncol=4,byrow = TRUE))
+expect_equal(as.numeric(pShiftCount(test10)),c(0,0,0,0,0,0,0,0,0,1,0,0,0))
+
+test11<-networkDynamic(edge.spells=matrix(c(0,1,1,2,
+                                           0,1,1,3,
+                                           0,1,1,4,
+                                           1,2,1,2
+),ncol=4,byrow = TRUE))
+expect_equal(as.numeric(pShiftCount(test11)),c(0,0,0,0,0,0,0,0,0,0,1,0,0))
+
+
+test12<-networkDynamic(edge.spells=matrix(c(0,1,1,2,
+                                           1,2,1,2,
+                                           1,2,1,3,
+                                           1,2,1,4
+),ncol=4,byrow = TRUE))
+expect_equal(as.numeric(pShiftCount(test12)),c(0,0,0,0,0,0,0,0,0,0,0,1,0))
+
+test13<-networkDynamic(edge.spells=matrix(c(0,1,1,2,
+                                            1,2,1,3
+),ncol=4,byrow = TRUE))
+expect_equal(as.numeric(pShiftCount(test13)),c(0,0,0,0,0,0,0,0,0,0,0,0,1))
+
+
+
+
+
+
+
